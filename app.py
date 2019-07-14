96b21c9c9a59d09322b172f12425dedafb0ed95f
Felipe Fonseca <fonsecafel@gmail.com>
2019-07-14 09:42:22 +0000

    Added a sidebar to the main page

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Game, User
+from flask import session as login_session
 
 app = Flask(__name__)
 
+# Connect to Database and create database session
+engine = create_engine('sqlite:///gamecatalog.db', connect_args={'check_same_thread': False})
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route('/')
-def hello_world():
-    return render_template('base.html')
+@app.route('/games/')
+def show_all_games():
+    categories = session.query(Category).all()
+    games = session.query(Game).order_by(Game.id.desc()).limit(10).all()
+    return render_template('games.html', games=games, categories=categories)
+
+@app.route('/games/<int:category_id>/')
+def show_games_by_category(category_id):
+    categories = session.query(Category).all()
+    games = session.query(Game).filter(Game.category_id == category_id).order_by(Game.id.desc()).all()
+
+    for category in categories:
+        if category.id == category_id:
+            chosen_category = category
+            break
+
+    return render_template('games.html', games=games, categories=categories, chosen_category=chosen_category)
+
 
 
 if __name__ == '__main__':
